f4a09e6b12dc610e7542d046a6af9d63095714e3
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-20 20:40:49 +0530

    added deleted logic

--- app.js
+++ app.js
@@ -74,6 +74,11 @@ app.post('/users/add', (req,res,next) => {
     })
 })
 
+app.delete('/user/delete/:id', (req,res,next) => {
+    client.del(req.params.id)
+    res.redirect('/')
+})
+
 app.listen('3000', () => {
     console.log('Server started on port 3000')
 })
